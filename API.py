842f318c73732844234a885490eed6f8a6555de3
Shan-tech <shankaranand4415@gmail.com>
2021-07-29 16:23:01 +0530

    fixed ui and bugs

--- API.py
+++ API.py
@@ -6,11 +6,11 @@ app = Flask('__name__')
 locator = Nominatim(user_agent='__name__')  # instantiate locator with app
 
 # Database config
-app.config['MYSQL_HOST'] = '____________'  # host
-app.config['MYSQL_USER'] = '____________'        # username
-app.config['MYSQL_PASSWORD'] = '____________'  # password
-app.config['MYSQL_DB'] = '____________'  # name of the database
-app.config['MYSQL_CURSORCLASS'] = '____________'  # cursor class
+app.config['MYSQL_HOST'] = 'localhost'  # host (localhost)
+app.config['MYSQL_USER'] = 'root'        # username (root)
+app.config['MYSQL_PASSWORD'] = 'Shan-tech'  # password
+app.config['MYSQL_DB'] = 'osm'  # name of the database
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # cursor class (DICTCURSOR)
 
 mysql = MySQL(app)      # instantiate app with sql
 
@@ -18,7 +18,7 @@ mysql = MySQL(app)      # instantiate app with sql
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        btn = request.form['button']
+        btn = request.form['button']  #add map
         print(btn)
         print(request.form['city'])
         if btn == "add":
@@ -30,13 +30,13 @@ def home():
             pincode = request.form['pincode']
             print(u_name, address, area, city, country, pincode)
             cur = mysql.connection.cursor()
-            cur.execute("INSERT INTO address_table(u_name,address,area,city,country,pincode) VALUES(%s,%s,%s,%s,%s,%s)",
+            cur.execute("INSERT INTO address_table(u_name,house_no,area,city,country,pincode) VALUES(%s,%s,%s,%s,%s,%s)",
                         (u_name, address, area, city, country, pincode))
             mysql.connection.commit()
             cur.close()
-            return '<h2>Added</h2><br><hr>To view map: Change url to http://127.0.0.1:5000/getUserId '
+            return redirect(url_for('get_id'))
         else:
-            return redirect(url_for('getUserId'))
+            return redirect(url_for('get_id'))
     return render_template('getUserInfo.html')
 
 # getting user's id to check map
@@ -45,19 +45,20 @@ def get_id():
     if request.method == 'POST':
         iid = request.form['id']
         print(iid)
+        print("@@@@@@@@@@@")
         return redirect(url_for('mapping', i=iid))
 
     # fetch database to display
-    headings = ("ID", "Name", "Address", "Area", "City", "Pincode", "Country") #since these are conctant i've hardcoded
+    headings = ("ID", "Name", "HouseNo.", "Area", "City", "Pincode", "Country") #since these are conctant i've hardcoded
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM address_table")
     data = cur.fetchall()
-    return render_template('id.html', header=headings, data=data)
+    return render_template('getUserId.html', header=headings, data=data)
 
 
 # Display user location in OSM
 @app.route('/mapping<i>', methods=['GET', 'POST'])
-def mapping(i):
+def mapping(i): #110
     # fetch address
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM address_table WHERE id=%s" % i)  # here i is the id of user(auto_increment primary key i DB)
@@ -74,14 +75,15 @@ def mapping(i):
     c_location = locator.geocode(area+","+pincode)
     c_lat = c_location.latitude
     c_long = c_location.longitude
-
+    print("@@@@@@@@@@@@@@@@@@")
+    print(c_lat,c_long);
     location = locator.geocode(area + ','+city+','+pincode+','+country)
     lat = location.latitude
     long = location.longitude
 
     print(lat, long)  # To check log
     print(Name)
-    return render_template('leaf.html', lati=lat, longi=long, c_lati=c_lat, c_longi=c_long, name=Name)
+    return render_template('leaflet.html', lati=lat, longi=long, c_lati=c_lat, c_longi=c_long, name=Name)
 
 
 # check main to run
